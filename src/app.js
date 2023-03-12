047dcdf77858a5fd5b28c1de3451a42653514ee6
Benoit Kekwandi <67283059+BenKekwandi@users.noreply.github.com>
2023-03-12 06:32:33 -0700

    chakra-ui

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Heading, Text } from '@chakra-ui/react';
+
+function App()
+{
+    return (<div>
+    <Heading>
+        Chakra UI design Application
+    </Heading>
+    <Text>
+        Lore Ipsum generated ...
+    </Text>
+    </div> 
+    );
+}
+
+export default App;
